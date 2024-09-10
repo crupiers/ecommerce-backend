bd5f6d4b78569898235a48202654084f2d683da5
Alejo <alejogiustinich@gmail.com>
2024-09-10 09:13:46 -0300

    modificacio  lombok

---
+++ ejemplo/src/main/java/programacion/ejemplo/exception/RecursoNoEncontradoExcepcion.java
@@ -0,0 +1,11 @@
+package programacion.ejemplo.exception;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class RecursoNoEncontradoExcepcion extends RuntimeException{
+    public RecursoNoEncontradoExcepcion(String mensaje){
+        super(mensaje);
+    }
+}
+
