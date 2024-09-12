dbaef258eae4903a8b022ca83d3a0da43d5d0244
frosskiel <frossascoezequiel@gmail.com>
2024-09-12 18:16:01 -0300

    cambio de 'NonNull'

--- src/main/java/programacion/eCommerceApp/controller/request/NewColorRequest.java
+++ src/main/java/programacion/eCommerceApp/controller/request/NewColorRequest.java
@@ -1,10 +1,10 @@
 package programacion.eCommerceApp.controller.request;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.NonNull;
+import jakarta.validation.constraints.NotNull;
 
 public record NewColorRequest( //debo pasar los datos necesarios para crear un color
-        @NonNull //verifico que no me pase un objeto nulo
+        @NotNull //verifico que no me pase un objeto nulo
         @NotBlank(message = "EL COLOR NO PUEDE SER ESPACIOS VACÍOS") //verifico que el string no sean solo espacios
         String nombre
 ) {
