a1634e1e4043f96581ce4d7f712b06d4557f4fd2
Agustin Sorti <agustinsorti@gmail.com>
2024-09-25 21:18:23 -0300

    correccion de mensaje

--- src/main/java/programacion/eCommerceApp/controller/request/NewCategoriaRequest.java
+++ src/main/java/programacion/eCommerceApp/controller/request/NewCategoriaRequest.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.Pattern;
 public record NewCategoriaRequest (
 
         @NotBlank(message = "El nombre de la categoria no puede estar vacio.")
-        @Pattern(regexp = "^[A-Za-z\s]+$", message = "La denominación solo debe contener letras y espacios")
+        @Pattern(regexp = "^[A-Za-z\s]+$", message = "El nombre solo debe contener letras y espacios")
         String nombre
 
 ) { }
