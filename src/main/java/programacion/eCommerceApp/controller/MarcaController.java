7a2f7a3c96023ffd64d7a77e7c9f1505a48cb019
frosskiel <frossascoezequiel@gmail.com>
2024-10-26 11:17:07 -0300

    refactor: marca controller

--- src/main/java/programacion/eCommerceApp/controller/MarcaController.java
+++ src/main/java/programacion/eCommerceApp/controller/MarcaController.java
@@ -1,81 +1,51 @@
 package programacion.eCommerceApp.controller;
 
 import jakarta.validation.Valid;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import org.springframework.web.server.ResponseStatusException;
 import programacion.eCommerceApp.controller.request.NewMarcaRequest;
 import programacion.eCommerceApp.controller.response.MarcaResponse;
-import programacion.eCommerceApp.mapper.MarcaMapper;
-import programacion.eCommerceApp.model.Marca;
 import programacion.eCommerceApp.service.IMarcaService;
-
 import java.util.List;
 
 @RestController
-@RequestMapping("/eCommerce")
+@RequestMapping("/ecommerce")
 @CrossOrigin(value=" http://localhost:8080")
 
 public class MarcaController {
 
-    private static final Logger logger = LoggerFactory.getLogger(MarcaController.class);
-
     @Autowired
     private IMarcaService modelService;
 
-    @GetMapping({"/marca"})
-    public List<MarcaResponse> getAll() {
-        logger.info("Entra y trae todas las marcas.");
+    @GetMapping({"/marcas"})
+    public List<MarcaResponse> listar() {
         return modelService.listar();
     }
 
-    @GetMapping("/marca/{id}")
+    @GetMapping("/marcas/{id}")
     public ResponseEntity<MarcaResponse> buscarPorId(@PathVariable Integer id){
-        Marca model = modelService.buscarPorId(id);
-
-        if(model == null || model.getEstado() == Marca.ELIMINADO){
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "NO SE ENCONTRÓ EL ID: "+id);
-        }
-        MarcaResponse marcaResponse = MarcaMapper.toMarcaResponse(model);
-        return ResponseEntity.ok(marcaResponse);
+        return modelService.buscarPorId(id);
     }
     
-    @PostMapping("/marca")
+    @PostMapping("/marcas")
     public MarcaResponse crear(@RequestBody @Valid NewMarcaRequest newMarcaRequest){
         return modelService.crear(newMarcaRequest);
     }
 
-    @PutMapping("/marca/actualizar/{id}")
+    @PutMapping("/marcas/actualizar/{id}")
     public MarcaResponse actualizar(@RequestBody @Valid NewMarcaRequest newMarcaRequest, @PathVariable Integer id){
         return modelService.actualizar(newMarcaRequest, id);
     }
 
-    @PutMapping("/marca/recuperar/{id}")
-    public ResponseEntity<Void> recuperar(@PathVariable Integer id) {
-        Marca model = modelService.buscarPorId(id);
-        if (model == null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "NO SE ENCONTRÓ EL ID: "+id);
-        }
-
-        modelService.recuperar(model); // Guarda el modelo actualizado
-        return ResponseEntity.ok().build(); // Respuesta vacía con estado 200 OK
-    }
-
-    @DeleteMapping("/marca/{id}")
+    @DeleteMapping("/marcas/{id}")
     public ResponseEntity<Void> eliminar(@PathVariable Integer id) {
-
-
-        Marca model = modelService.buscarPorId(id);
-        if (model == null){
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "NO SE ENCONTRÓ EL ID: "+id);
-        }
-
-        modelService.eliminar(model);
-        return ResponseEntity.ok().build();
+        return modelService.eliminar(id);
     }
+
+    @PutMapping("/marcas/recuperar/{id}")
+    public ResponseEntity<Void> recuperar(@PathVariable Integer id) {
+        return modelService.recuperar(id);
+    }
+
 }
