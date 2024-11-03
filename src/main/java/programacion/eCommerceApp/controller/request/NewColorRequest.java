47b5341f89cf6698baa103c21934620f7839dbae
frosskiel <frosskiel@gmail.com>
2024-11-03 18:44:50 -0300

    add: mensaje de longitud de caracteres en el nombre de entidades

--- src/main/java/programacion/eCommerceApp/controller/request/NewColorRequest.java
+++ src/main/java/programacion/eCommerceApp/controller/request/NewColorRequest.java
@@ -2,9 +2,11 @@ package programacion.eCommerceApp.controller.request;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record NewColorRequest(
         @NotNull
+        @Size(min = 2, max = 24, message = "EL NOMBRE DEBE IR DE ENTRE 2 Y 24 CARACTERES")
         @Pattern(regexp = "^[a-zA-Z0-9]+(\\s[a-zA-Z0-9]+)*$", message = "SÓLO PUEDEN HABER LETRAS Y NÚMEROS, NO TIENE QUE HABER ESPACIOS DOBLES NI ESPACIO AL INICIO NI AL FINAL")
         String nombre,
 
