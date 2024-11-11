037c4dc1dbfdc1e7cc5a3e0c0f865df6dbcc277a
frosskiel <frosskiel@gmail.com>
2024-11-11 10:59:37 -0300

    add: validaciones de nombre y contraseña de usuario && fix: mensaje longitud de nombre de entidades

--- src/main/java/programacion/eCommerceApp/controller/request/NewLoginRequest.java
+++ src/main/java/programacion/eCommerceApp/controller/request/NewLoginRequest.java
@@ -1,13 +1,19 @@
 package programacion.eCommerceApp.controller.request;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record NewLoginRequest(
 
-    @NotBlank
-    String nombre,
+        @NotNull
+        @Size(min = 2, max = 32, message = "EL NOMBRE DEBE IR DE ENTRE 2 Y 32 CARACTERES")
+        @Pattern(regexp = "^([a-z]+|[0-9]+)(-([a-z]|[0-9])+)*$", message = "EL NOMBRE DEBE ESTAR CONFORMADO POR PALABRAS DE SÓLO LETRAS MINÚSCULAS O DE SÓLO NÚMEROS, LAS PALABRAS DEBEN ESTAR SEPARADAS CON GUIÓN COMÚN (-)")
+        String nombre,
 
-    @NotBlank
-    String contrasenia
+        @NotNull
+        @Size(min = 8, max = 64, message = "LA CONTRASEÑA DEBE IR DE ENTRE 8 Y 64 CARACTERES")
+        @Pattern(regexp = "^((([^\\s]*[a-z]+[^\\s]*[A-Z]+[^\\s]*[0-9]+[^\\s]*)|([^\\s]*[a-z]+[^\\s]*[0-9]+[^\\s]*[A-Z]+[^\\s]*))|(([^\\s]*[A-Z]+[^\\s]*[a-z]+[^\\s]*[0-9]+[^\\s]*)|([^\\s]*[A-Z]+[^\\s]*[0-9]+[^\\s]*[a-z]+[^\\s]*))|(([^\\s]*[0-9]+[^\\s]*[a-z]+[^\\s]*[A-Z]+[^\\s]*)|([^\\s]*[0-9]+[^\\s]*[A-Z]+[^\\s]*[a-z]+[^\\s]*)))$", message = "LA CONTRASEÑA DEBE CONTENER AL MENOS UNA MINÚSCULA, UNA MAYÚSCULA Y UN NÚMERO, NO PUEDE CONTENER ESPACIOS")
+        String contrasenia
 
 ) { }
--- src/main/java/programacion/eCommerceApp/controller/request/NewMarcaRequest.java
+++ src/main/java/programacion/eCommerceApp/controller/request/NewMarcaRequest.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.Size;
 
 public record NewMarcaRequest(
         @NotNull
-        @Size(min = 2, max = 32, message = "EL NOMBRE DEBE IR DE ENTRE 2 Y 24 CARACTERES")
+        @Size(min = 2, max = 32, message = "EL NOMBRE DEBE IR DE ENTRE 2 Y 32 CARACTERES")
         @Pattern(regexp = "^[a-zA-Z0-9]+(\\s[a-zA-Z0-9]+)*$", message = "SÓLO PUEDEN HABER LETRAS Y NÚMEROS, NO TIENE QUE HABER ESPACIOS DOBLES NI ESPACIO AL INICIO NI AL FINAL")
         String nombre,
 
