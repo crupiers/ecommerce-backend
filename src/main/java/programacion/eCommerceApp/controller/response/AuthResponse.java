fe74782227846aa9aef1f121309bb26dc2f4fd7d
Nacho <ignacio.rover@gmail.com>
2024-12-18 15:34:11 -0300

    change: authResponse

--- src/main/java/programacion/eCommerceApp/controller/response/AuthResponse.java
+++ src/main/java/programacion/eCommerceApp/controller/response/AuthResponse.java
@@ -1,12 +1,4 @@
 package programacion.eCommerceApp.controller.response;
 
 public record AuthResponse(String jwt, Integer id, String nombre, String contrasenia, String rol, Integer estado) {
-    public AuthResponse {
-        this.jwt = jwt;
-        this.id = id;
-        this.nombre = nombre;
-        this.contrasenia = contrasenia;
-        this.rol = rol;
-        this.estado = estado;
-    }
 }
