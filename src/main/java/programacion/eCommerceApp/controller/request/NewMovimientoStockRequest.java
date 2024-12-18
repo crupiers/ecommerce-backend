5f2e733a596fe74d314204e4b74b36601df3db43
alejo giustinich <alejogiustinich@gmail.com>
2024-12-18 15:08:09 -0300

    add: .

--- src/main/java/programacion/eCommerceApp/controller/request/NewMovimientoStockRequest.java
+++ src/main/java/programacion/eCommerceApp/controller/request/NewMovimientoStockRequest.java
@@ -1,12 +1,14 @@
 package programacion.eCommerceApp.controller.request;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record NewMovimientoStockRequest(
 
         @NotNull
         Integer cantidad,
 
+        @Size(min = 2, max = 32, message = "EL MOTIVO DEBE IR DE ENTRE 2 Y 32 CARACTERES")
         @NotNull
         String motivo,
 
