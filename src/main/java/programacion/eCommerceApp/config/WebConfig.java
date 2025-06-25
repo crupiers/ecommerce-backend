ec30e806c780615f74e3da1d3fe1eed21065c379
frosskiel <frosskiel@gmail.com>
2025-06-25 11:56:32 -0300

    fix: ip server

--- src/main/java/programacion/eCommerceApp/config/WebConfig.java
+++ src/main/java/programacion/eCommerceApp/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://192.168.1.76:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
